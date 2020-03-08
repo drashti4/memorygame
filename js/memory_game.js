1086964a90f13db336cc13a766fa974f0965a6f9
Drashti Kaushikkumar Pandya <dpandya@isimtl.local>
2020-03-08 13:25:40 -0400

    couple radom img done

--- js/memory_game.js
+++ js/memory_game.js
@@ -1,26 +1,31 @@
 'use strict'
 
-window.addEventListener('DOMContentLoaded', (event) => {
 
-    let index, rand;
-    var excludeArray = [];
-    let array = [];
-    var counters = [];
+let imageIndex;
+let arrayExcludeNumbers = [];
+let arrayRandomImageIndex = [];
+let counters = [];
+for (let i = 0; i < 17; i++) {
+    counters[i] = 0;
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
 
     let defaultButtons = document.getElementsByClassName('default');
 
     for (let i = 0; i < defaultButtons.length; i++) {
+
+        let defaultBtn = this;
         defaultButtons[i].addEventListener('click', () => {
             defaultButtons[i].src = getRandomImage();
+        }, {
+            once: true
         });
     }
 
 
-    function getRandomImage() {
 
-        for (let i = 0; i < 17; i++) {
-            counters[i] = 0;
-        }
+    function getRandomImage() {
 
         let listImages = ["file:///C:/isi/JavaScript/MemoryGame/images/arrows.svg",
             "file:///C:/isi/JavaScript/MemoryGame/images/back.svg",
@@ -32,44 +37,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
             "file:///C:/isi/JavaScript/MemoryGame/images/turn.svg"
         ];
 
-        let flag = false;
-        let i = 0;
-        index = getRandomInt(8, excludeArray);
-        array.push(index);
-
         do {
-            if (index == array[i]) {
+            imageIndex = getRandomInt(listImages.length, arrayExcludeNumbers);
+        } while (counters[imageIndex] >= 2);
 
-                console.log("Incrementing old counter ", counters[index], "for index", index);
-                counters[index]++;
-                console.log("Incrementing new counter ", counters[index], "for index", index);
+        arrayRandomImageIndex.push(imageIndex);
+        counters[imageIndex]++;
 
-                if (counters[index] > 2) {
-
-                    console.log("DISCARD IMAGE array index", index, "With counter", counters[index]);
-                    excludeArray.push(index);
-                    index = getRandomInt(9, excludeArray);
-                    for (let i = 0; i < excludeArray.length; i++) {
-                        console.log("ADDED 2 TIMES " + excludeArray[i]);
-                    }
-                    console.log("NEW DIS IMAGE array index ", index);
-                    array.push(index);
-                    flag = true;
-                    break;
-                }
-            }
-            i++;
-        } while (i < array.length);
-        if (flag) {
-            console.log("New index is " + index);
+        if (counters[imageIndex] === 2) {
+            arrayExcludeNumbers.push(imageIndex);
         }
-        for (let i = 0; i < 17; i++) {
-            if (counters[i] != 0)
-                console.log("Counter " + i + " val " + counters[i]);
-        }
-        return listImages[index];
+        console.log("Counter " + imageIndex + " val " + counters[imageIndex]);
+        return listImages[imageIndex];
     }
-
 
     function getRandomInt(max, excludeArray) {
 
@@ -89,10 +69,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         if (flag) {
             let temp = getRandomInt(max, excludeArray);
-            console.log("Again number generated is " + temp);
             return temp;
         } else {
-            console.log("Generated number is " + randomNumber);
             return randomNumber;
         }
     }
