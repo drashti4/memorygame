644088d1501ecebe48cdfb50c5bc36c96bf8fc24
Drashti Kaushikkumar Pandya <dpandya@isimtl.local>
2020-03-11 21:51:02 -0400

    Image match - Timer set

--- js/memory_game.js
+++ js/memory_game.js
@@ -1,41 +1,102 @@
 'use strict'
 
+$(window).load(function () {
 
-let imageIndex;
-let arrayExcludeNumbers = [];
-let arrayRandomImageIndex = [];
-let counters = [];
-for (let i = 0; i < 17; i++) {
-    counters[i] = 0;
-}
+    let imageIndex;
+    let arrayExcludeNumbers = [];
+    let arrayRandomImageIndex = [];
+    let arrayGeneratedImage = [];
+    let counters = [];
+    let isOpenedOnce = false
+    /* Initialize all image counter by 0 */
 
-window.addEventListener('DOMContentLoaded', (event) => {
 
-    let defaultButtons = document.getElementsByClassName('default');
+    const listImages = ["file:///C:/isi/JavaScript/MemoryGame/images/arrows.svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/back.svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/down-arrow(1).svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/down-arrow.svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/refresh.svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/right.svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/turn(1).svg",
+        "file:///C:/isi/JavaScript/MemoryGame/images/turn.svg"
+    ];
+
+    let defaultButtons = document.getElementsByClassName('btnDefault');
+    console.log('LOG ' + defaultButtons.length);
+
+    for (let j = 0; j < defaultButtons.length; j++) {
+        counters[j] = 0;
+        defaultButtons[j].setAttribute('isFirstClick', false);
+    }
 
     for (let i = 0; i < defaultButtons.length; i++) {
 
-        let defaultBtn = this;
-        defaultButtons[i].addEventListener('click', () => {
-            defaultButtons[i].src = getRandomImage();
-        }, {
-            once: true
+        defaultButtons[i].addEventListener('click', function () {
+
+            console.log('check value ' + defaultButtons[i].getAttribute('isFirstClick')); // print false
+            if (defaultButtons[i].getAttribute('isFirstClick') == false) {
+                console.log('first')
+                addFirstClickListner(defaultButtons[i], defaultButtons, i);
+            }
+
+            if (!defaultButtons[i].getAttribute('isFirstClick')) {
+                console.log('first')
+                addFirstClickListner(defaultButtons[i], defaultButtons, i);
+            }
+
+            if (defaultButtons[i].getAttribute('isFirstClick')) {
+                console.log('second')
+                addMultiClickListener(defaultButtons[i], defaultButtons, i);
+            }
         });
     }
 
+    function addFirstClickListner(selectedImage, defaultButtons, index) {
+        console.log('first click index is ' + index);
+        selectedImage.src = getRandomImage();
+        arrayGeneratedImage.push(defaultButtons[index].src);
+        selectedImage.setAttribute('status', 'opened');
+        let timer = addTimer(selectedImage);
+        checkSameImage(defaultButtons, index, timer);
+        selectedImage.setAttribute('isFirstClick', false);
+    }
 
+    function addMultiClickListener(selectedImage, defaultButtons, index) {
+        console.log('second click index is ' + index);
+        selectedImage.src = arrayGeneratedImage[index];
+        selectedImage.setAttribute('status', 'opened');
+        selectedImage.setAttribute('isFirstClick', true);
+        let timer = addTimer(selectedImage);
+        checkSameImage(defaultButtons, index, timer);
 
+    }
+
+    function addTimer(selectedImage) {
+        return setInterval(function () {
+            selectedImage.setAttribute('status', 'closed');
+            selectedImage.src = "images/plain-circle.svg";
+        }, 5000);
+
+    }
+
+    function checkSameImage(defaultButtons, index, timer) {
+        for (let i = 0; i < defaultButtons.length; i++) {
+            if (defaultButtons[i].getAttribute('status') === 'opened') {
+                if (i != index) {
+                    if (defaultButtons[i].src == defaultButtons[index].src) {
+                        console.log("Match found with " + i);
+                        defaultButtons[i].src = arrayGeneratedImage[index];
+                        defaultButtons[i].setAttribute('status', 'solved');
+                        defaultButtons[index].setAttribute('status', 'solved');
+                        clearTimeout(timer);
+                    }
+                }
+            }
+        }
+    }
+
+    /* This function will return random image from array.*/
     function getRandomImage() {
-
-        let listImages = ["file:///C:/isi/JavaScript/MemoryGame/images/arrows.svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/back.svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/down-arrow(1).svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/down-arrow.svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/refresh.svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/right.svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/turn(1).svg",
-            "file:///C:/isi/JavaScript/MemoryGame/images/turn.svg"
-        ];
 
         do {
             imageIndex = getRandomInt(listImages.length, arrayExcludeNumbers);
@@ -47,12 +108,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
         if (counters[imageIndex] === 2) {
             arrayExcludeNumbers.push(imageIndex);
         }
-        console.log("Counter " + imageIndex + " val " + counters[imageIndex]);
         return listImages[imageIndex];
     }
 
+    /*This function will take parameter as limit and excluded numbers and return random number */
     function getRandomInt(max, excludeArray) {
-
         let randomNumber = Math.floor(Math.random() * Math.floor(max));
         let flag = false;
 
